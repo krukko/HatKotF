deeb99305e75aaaf850c4fc23e775563d2a1384d
Krukko <kukkoronia@gmail.com>
2019-11-20 13:53:13 +0200

    Fixed error.

--- HatKotF/Assets/Scripts/BattleScripts/Tt_BattleButton.cs
+++ HatKotF/Assets/Scripts/BattleScripts/Tt_BattleButton.cs
@@ -1,111 +1,111 @@
-﻿////Battle Button for tier2 emotions.
+﻿//Battle Button for tier2 emotions.
 
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class Tt_BattleButton : MonoBehaviour
-//{
-//    public int ID;
-//    public string name;
-//    internal object onClick;
+public class Tt_BattleButton : MonoBehaviour
+{
+    public int ID;
+    public string name;
+    internal object onClick;
 
-//    //public EmotionList emotionList;
-//    public BattleManager battleManager;
+    public EmotionListCopy emotionList;
+    public BattleManager battleManager;
 
-//    protected bool endGame;
-//    public bool winGame;
-//    public bool loseGame;
+    protected bool endGame;
+    public bool winGame;
+    public bool loseGame;
 
-//    public int currentEmotionID;
+    public int currentEmotionID;
 
-//    public void Attack(Tt_BattleButton button)
-//    {
-//        int buttonNumber = button.giveButtonID();
-//        int comparableEmotionID = emotionList.currentEmotionID;
-//        currentEmotionID = comparableEmotionID;
+    public void Attack(Tt_BattleButton button)
+    {
+        int buttonNumber = button.giveButtonID();
+        int comparableEmotionID = emotionList.currentEmotionID;
+        currentEmotionID = comparableEmotionID;
 
-//        int currentPlayerHP = battleManager.player.GiveHP();
-//        int currentEnemyHP = battleManager.enemy.GiveHP();
-//        int CompareTier1 = TakeNDigits(comparableEmotionID, 1); //the base emotion
+        int currentPlayerHP = battleManager.player.GiveHP();
+        int currentEnemyHP = battleManager.enemy.GiveHP();
+        int CompareTier1 = TakeNDigits(comparableEmotionID, 1); //the base emotion
 
-//        int compareButton1 = TakeNDigits(comparableEmotionID, buttonNumber); //the int for the first number of the button (to compare with the base emotion)
-//        comparableEmotionID = TakeNDigits(comparableEmotionID, battleManager.battleTier);
+        int compareButton1 = TakeNDigits(comparableEmotionID, buttonNumber); //the int for the first number of the button (to compare with the base emotion)
+        comparableEmotionID = TakeNDigits(comparableEmotionID, battleManager.battleTier);
 
-//        if (compareButton1 == CompareTier1)
-//        {
-//            if (comparableEmotionID == buttonNumber)
-//            {
-//                if (battleManager.enemy.AmIAlive() == true)
-//                {
-//                    battleManager.DamageToEnemy();
-//                }
-//                if (battleManager.enemy.AmIAlive() == false)
-//                {
-//                    winGame = true;
-//                    endGame = true;
-//                    battleManager.Win();
-//                }
-//                else
-//                {
-//                    emotionList.GameRoundEmotions();
-//                    battleManager.BackClicked();
-//                }
-//            }
+        if (compareButton1 == CompareTier1)
+        {
+            if (comparableEmotionID == buttonNumber)
+            {
+                if (battleManager.enemy.AmIAlive() == true)
+                {
+                    battleManager.DamageToEnemy();
+                }
+                if (battleManager.enemy.AmIAlive() == false)
+                {
+                    winGame = true;
+                    endGame = true;
+                    battleManager.Win();
+                }
+                else
+                {
+                    emotionList.GameRoundEmotions();
+                    battleManager.BackClicked();
+                }
+            }
 
-//            if (comparableEmotionID != buttonNumber)
-//            {
-//                battleManager.damageModifier = 0.75f;
-//            }
-//            else
-//            {
-//                battleManager.DamageToPlayer();
+            if (comparableEmotionID != buttonNumber)
+            {
+                battleManager.damageModifier = 0.75f;
+            }
+            else
+            {
+                battleManager.DamageToPlayer();
 
-//                if (battleManager.player.AmIAlive() == false)
-//                {
-//                    loseGame = true;
-//                    endGame = true;
-//                    battleManager.GameOver();
-//                }
-//                else
-//                {
-//                    emotionList.GameRoundEmotions();
-//                    battleManager.BackClicked();
-//                }
-//            }
-//        }
-//    }
+                if (battleManager.player.AmIAlive() == false)
+                {
+                    loseGame = true;
+                    endGame = true;
+                    battleManager.GameOver();
+                }
+                else
+                {
+                    emotionList.GameRoundEmotions();
+                    battleManager.BackClicked();
+                }
+            }
+        }
+    }
 
 
-//    //for calculating the base emotion in tier2
-//    public int TakeNDigits(int emotionID, int N)
-//    {
-//        //N = NofDigits;
-//        //emotionID = emotionList.currentEmotionID;
+    //for calculating the base emotion in tier2
+    public int TakeNDigits(int emotionID, int N)
+    {
+        //N = NofDigits;
+        //emotionID = emotionList.currentEmotionID;
 
-//        if (emotionList.currentEmotionID <= N)
-//        {
-//            return (int)Mathf.FloorToInt((emotionList.currentEmotionID / Mathf.Pow(10, N - N)));
-//        }
-//        else
-//        {
-//            return emotionList.currentEmotionID;
-//        }
-//    }
+        if (emotionList.currentEmotionID <= N)
+        {
+            return (int)Mathf.FloorToInt((emotionList.currentEmotionID / Mathf.Pow(10, N - N)));
+        }
+        else
+        {
+            return emotionList.currentEmotionID;
+        }
+    }
 
-//    public Tt_BattleButton(int ID, string name)
-//    {
-//        this.ID = ID;
-//        this.name = name;
-//    }
+    public Tt_BattleButton(int ID, string name)
+    {
+        this.ID = ID;
+        this.name = name;
+    }
 
-//    public int giveButtonID()
-//    {
-//        return this.ID;
-//    }
+    public int giveButtonID()
+    {
+        return this.ID;
+    }
 
-//    public string giveButtonName()
-//    {
-//        return this.name;
-//    }
-//}
+    public string giveButtonName()
+    {
+        return this.name;
+    }
+}
